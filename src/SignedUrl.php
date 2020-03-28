ab7694ca0fa31f4daaf9aa13ae0a015f12dcb933
Kieran Ashley <kieran.ashley@wearetwogether.com>
2020-03-28 17:25:18 +0000

    Specify return types

--- src/SignedUrl.php
+++ src/SignedUrl.php
@@ -15,36 +15,36 @@ class SignedUrl
         return $this;
     }
 
-    public function withConfig(string $config)
+    public function withConfig(string $config): SignedUrl
     {
         $this->config = $config;
         return $this;
     }
 
-    public function withKey(string $key)
+    public function withKey(string $key): SignedUrl
     {
         $this->key = $key;
         return $this;
     }
 
-    public function withExpiry(int $expiry)
+    public function withExpiry(int $expiry): SignedUrl
     {
         $this->expiry = $expiry;
         return $this;
     }
 
-    public function withSource(string $source)
+    public function withSource(string $source): SignedUrl
     {
         $this->source = $source;
         return $this;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return URLSigner::sign($this->url,$this->config,$this->source,$this->key,$this->expiry ?: time()+120);
     }
 
-    public function get()
+    public function get(): string
     {
         return $this->__toString();
     }
