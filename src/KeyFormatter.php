e311ed5a07de0f61aa36695273d1bdee210f858a
Kieran Ashley <kieran@actinity.com>
2022-07-11 10:06:21 +0100

    Add a toString method to the KeyFormatter

--- src/KeyFormatter.php
+++ src/KeyFormatter.php
@@ -3,7 +3,7 @@ namespace Twogether\LaravelURLSigner;
 
 class KeyFormatter
 {
-    public static function fromString($string,$is_private = false)
+    public static function fromString(string $string,$is_private = false): string
     {
         if(substr($string,0,12) !== '-----BEGIN P') {
             $string = trim($string);
@@ -15,4 +15,14 @@ class KeyFormatter
 
         return $string;
     }
+
+	public static function toString(string $key): string
+	{
+		$lines = explode("\n",trim($key));
+		if(count($lines) && substr($lines[0],0,10) === '-----BEGIN') {
+			$lines = array_slice($lines,1,-1);
+		}
+		$lines = array_map('trim',$lines);
+		return implode("",$lines);
+	}
 }
